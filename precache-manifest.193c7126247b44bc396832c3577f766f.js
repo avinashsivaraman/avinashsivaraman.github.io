59471ef03bdaca854f1a39f81f1dda488f405f95
Avinash <avinashcegian95@gmail.com>
2019-05-17 23:03:16 -0700

    React version update

---
+++ precache-manifest.193c7126247b44bc396832c3577f766f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc034a97bcf7aeb4bd7b99bc1474c20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b3fb2ba74c2325c9720",
+    "url": "/static/css/main.2e646b76.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "2b3fb2ba74c2325c9720",
+    "url": "/static/js/main.0f2e2ae5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
